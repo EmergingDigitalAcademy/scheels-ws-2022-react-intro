e87e31f21949c10b7925d097e9cf5b9c183d0d25
masonleonhart <mason.leonhart@gmail.com>
2022-12-08 19:03:19 -0600

    removed unused css import

--- notes/my-app/src/index.js
+++ notes/my-app/src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./static/00-index.css";
-import "./static/02-functional-components.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
